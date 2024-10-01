c28752ce73cbb9f4e27076e9e50265b021702a25
devbiah <emmyisilva@gmail.com>
2024-10-01 15:59:07 -0300

    portfolio finished.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MainPortfolio/', // ajuste para o nome do seu repositório
   plugins: [react()],
- build: {
-        chunkSizeWarningLimit: 1600
- },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     sourcemap: true,
   },
-})
+});
